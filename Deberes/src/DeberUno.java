60a37e1dae2585a49f48aee797ab49416be80dc9
dakookie <damaris.suquillo@epn.edu.ec>
2022-12-04 22:31:01 -0500

    [F1] Init class

---
+++ Deberes/src/DeberUno.java
@@ -0,0 +1,10 @@
+package Deberes.src;
+import java.util.Scanner;
+public class DeberUno {
+    public static void main(String[] args) {
+        
+        Scanner ingresoConsola = new Scanner(System.in);
+        
+        System.out.println("Hola mundo");
+    }
+}
