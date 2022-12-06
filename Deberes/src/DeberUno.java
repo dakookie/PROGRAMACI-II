08e9bc7c29a912be54083198cd35c97520e1f467
dakookie <damaris.suquillo@epn.edu.ec>
2022-12-06 00:16:14 -0500

    Se logróoooooooooooooooooo, fin del programa

--- Deberes/src/DeberUno.java
+++ Deberes/src/DeberUno.java
@@ -2,41 +2,38 @@ package Deberes.src;
 import java.util.Scanner;
 import java.util.Arrays;
 public class DeberUno {
-    /* 
-    private static int busquedarreglo(String[] izquierda, int ingreso) {
-        int indice = 0;
-        for (int i=0; i < izquierda.length; i++){
-            if(izquierda[i].equals(ingreso)){
-                indice = i;
-            }; 
-        }
-        return indice;
-    }
-    */
     public static void main(String[] args) {
-        
         //Definir variables
         String[] izquierda = {"O","L","C","U"};
         String[] derecha = {"","","",""};
         int ingreso;
-
         boolean bandera = true;
         Scanner ingresoConsola = new Scanner(System.in);
-        
 
         //Operaciones
-        
         System.out.println("Bienvenido al juego del lobo, caperucita y las uvas");
+        System.out.println("Reglas");
+        System.out.println("El lobo no se puede quedar solo con la caperucita porque la come");
+        System.out.println("Caperucita no se puede quedar sola con las uvas porque las come");
         while(bandera == true){
-            
-            System.out.println("Digite 1 si desea cruzar de izquierda a derecha al observador y uvas, y se queden las uvas");
-            System.out.println("Digite 2 si desea cruzar de izquierda a derecha al observador y lobo, y se quede el lobo");
+            System.out.println("Digite 1 si desea cruzar de izquierda a derecha al observador y uvas");
+            System.out.println("Digite 2 si desea cruzar de izquierda a derecha al observador y lobo");
             System.out.println("Digite 3 si desea cruzar de izquierda a derecha al observador y caperucita");
             System.out.println("Digite 4 si desea cruzar de derecha a izquierda al observador");
-            //System.out.println("Digite 5 si desea cruzar de derecha a izquierda a caperucita");
-            //System.out.println("Digite 6 si desea cruzar de derecha a izquierda al lobo");
-            //System.out.println("Digite 7 si desea cruzar de derecha a izquierda a las uvas");
+            System.out.println("Digite 5 si desea cruzar de derecha a izquierda al observador y uvas");
+            System.out.println("Digite 6 si desea cruzar de derecha a izquierda al observador y lobo");
+            System.out.println("Digite 7 si desea cruzar de derecha a izquierda al observador y caperucita");
             ingreso = Integer.parseInt(ingresoConsola.nextLine());
+            if(ingreso==1 && (izquierda[1].equals("L")&&(izquierda[2].equals("C")))){
+                System.out.println("Perdiste");
+                System.out.println("El lobo se comió a caperucita");
+                bandera = false;
+            }
+            if(ingreso==2 && (izquierda[2].equals("C")&&(izquierda[3].equals("U")))){
+                System.out.println("Perdiste");
+                System.out.println("Caperucita se comió las uvas");
+                bandera = false;
+            }
             switch (ingreso) {
                 case 1:
                     izquierda[0] = " ";
@@ -46,9 +43,6 @@ public class DeberUno {
                     System.out.print(Arrays.toString(izquierda));
 				    System.out.print(" >>> ");
 				    System.out.println(Arrays.toString(derecha));
-                    //busquedarreglo(izquierda,ingreso);
-                    //System.out.println(busquedarreglo(izquierda, ingreso));
-                    
                     break;
                 case 2:
                     izquierda[0] = " ";
@@ -58,9 +52,6 @@ public class DeberUno {
                     System.out.print(Arrays.toString(izquierda));
 				    System.out.print(" >>> ");
 				    System.out.println(Arrays.toString(derecha));
-                    //busquedarreglo(izquierda,ingreso);
-                    //System.out.println(busquedarreglo(izquierda, ingreso));
-                    
                     break;
                 case 3:
                     izquierda[0] = " ";
@@ -70,9 +61,6 @@ public class DeberUno {
                     System.out.print(Arrays.toString(izquierda));
 				    System.out.print(" >>> ");
 				    System.out.println(Arrays.toString(derecha));
-                    //busquedarreglo(izquierda,ingreso);
-                    //System.out.println(busquedarreglo(izquierda, ingreso));
-                    
                     break;
                 case 4:
                     izquierda[0] = "O";
@@ -81,54 +69,47 @@ public class DeberUno {
 				    System.out.print(" >>> ");
 				    System.out.println(Arrays.toString(derecha));
                     break;
-                /*
                 case 5:
                     izquierda[0] = "O";
+                    izquierda[3] = "U";
                     derecha[0] = " ";
+                    derecha[3] = " ";
                     System.out.print(Arrays.toString(izquierda));
 				    System.out.print(" >>> ");
 				    System.out.println(Arrays.toString(derecha));
-                    busquedarreglo(izquierda,ingreso);
-                    System.out.println(busquedarreglo(izquierda, ingreso));
-                    
                     break;
                 case 6:
                     izquierda[0] = "O";
+                    izquierda[1] = "L";
                     derecha[0] = " ";
+                    derecha[1] = " ";
                     System.out.print(Arrays.toString(izquierda));
 				    System.out.print(" >>> ");
 				    System.out.println(Arrays.toString(derecha));
-                    busquedarreglo(izquierda,ingreso);
-                    System.out.println(busquedarreglo(izquierda, ingreso));
-                    
                     break;
                 case 7:
                     izquierda[0] = "O";
+                    izquierda[2] = "C";
                     derecha[0] = " ";
+                    derecha[2] = " ";
                     System.out.print(Arrays.toString(izquierda));
 				    System.out.print(" >>> ");
 				    System.out.println(Arrays.toString(derecha));
-                    busquedarreglo(izquierda,ingreso);
-                    System.out.println(busquedarreglo(izquierda, ingreso));
-                    
                     break;
-                */
                 default:
                     System.out.println("El numero ingresado es incorrecto");
                     bandera = false;
                     break;
             }
-              
-            
-            // = false;
         }
-  
-       //caso 4
-       System.out.println("Se presentan los resultados");
+       //caso 
+        if((derecha[0].equals("O"))&&(derecha[1].equals("L"))&&(derecha[2].equals("C"))&&derecha[3].equals("U")){
+        System.out.println("Has ganado el juego");
+        System.out.println("Se presentan los resultados");
         System.out.print(Arrays.toString(izquierda));
         System.out.print(" >>> ");
         System.out.println(Arrays.toString(derecha));
-        
-        
+        bandera = false;
     }
+ }
 }
